c6af3b2c6eb9ade7840bab70e4a0aa7873162f40
WysllenLudolf <leeludolf.3dart@gmail.com>
2025-09-16 18:40:26 +0000

    criei as entradas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   char estado1 = "Amazonas";
-  char codigodacartaA[50] = "A01";
+  char codigodacartaA[10] = "A01";
   char nomedacidadeA[50] = "Manaus";
   int populacaoA = 2303732;
   float areakm2A = 11.401092;
@@ -16,21 +16,50 @@ int main() {
   int pontoturisticoA = 15;
 
   char estado2 = "Bahia";
-  char codigodacartaB[50] = "B01";
+  char codigodacartaB[10] = "B01";
   char nomedacidadeB[50] = "Salvador";
   int populacaoB = 2564204;
   float areakm2B = 693.453;
   float pibB = 62.954000000;
   int pontoturisticoB = 16;
   
-
-
-
   // Área para entrada de dados
 
-  printf("Digite o nome do estado: ");
+  printf("Digite o nome do Estado: \n");
+  scanf("%s", &estado1, &estado2);
+
+  printf("Digite a cidade: \n");
+  scanf("%s", &nomedacidadeA, nomedacidadeB);
+
+  printf("Informe o código da carta: \n");
+  scanf("%s", &codigodacartaA, &codigodacartaB);
+
+  printf("Qual a população da cidade: \n");
+  scanf("%d", &populacaoA, &populacaoB);
+
+  printf("Qual o tamanho da cidade em km2: \n");
+  scanf("%f", &areakm2A, &areakm2B);
+
+  printf("Qual o PIB da cidade: \n");
+  scanf("%f", &pibA, &pibB);
+
+  printf("Quantos são os pontos turísticos dessa cidade: \n");
+  scanf("%d", &pontoturisticoA, &pontoturisticoB);
+
+  printf("Estado: %s\n", estado1);
+  printf("Cidade: %s\n", nomedacidadeA);
+  printf("Código: %s\n", codigodacartaA);
+  printf("População: %f\n", populacaoA);
+  printf("Área Km²: %f\n", areakm2A);
+  printf("PIB: %f\n", pibA);
+  printf("Pontos Turísticos: %d\n", pontoturisticoA, pontoturisticoB);
+
+
 
   // Área para exibição dos dados da cidade
+
+  
+
 
 return 0;
 } 
