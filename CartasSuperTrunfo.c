967d30e63033984b142017478b1c1a30f16f1a8d
WysllenLudolf <leeludolf.3dart@gmail.com>
2025-09-16 17:44:47 +0000

    Criei as variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,28 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  char estado1 = "Amazonas";
+  char codigodacartaA[50] = "A01";
+  char nomedacidadeA[50] = "Manaus";
+  int populacaoA = 2303732;
+  float areakm2A = 11.401092;
+  float pibA = 35.000000000;
+  int pontoturisticoA = 15;
+
+  char estado2 = "Bahia";
+  char codigodacartaB[50] = "B01";
+  char nomedacidadeB[50] = "Salvador";
+  int populacaoB = 2564204;
+  float areakm2B = 693.453;
+  float pibB = 62.954000000;
+  int pontoturisticoB = 16;
+  
+
+
+
   // Área para entrada de dados
+
+  printf("Digite o nome do estado: ");
 
   // Área para exibição dos dados da cidade
 
